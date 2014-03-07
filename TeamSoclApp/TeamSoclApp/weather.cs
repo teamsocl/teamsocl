d61f77fa698a4807ee09522a018257017a84ab1d
teamsocl <teamsocl@outlook.com>
2014-03-07 11:40:58 -0800

    Bryan Wx Update
    added adjusted

--- TeamSoclApp/TeamSoclApp/weather.cs
+++ TeamSoclApp/TeamSoclApp/weather.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace TeamSoclApp
 {
     class weather
     {
+        static string weather(string zip1)  // weather function to be implemented in full later - 90%
+        {
+            string answer = "";
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("http://xml.weather.yahoo.com/forecastrss?p=" + zip1);
+
+            // Set up namespace manager for XPath  
+            XmlNamespaceManager ns = new XmlNamespaceManager(doc.NameTable);
+            ns.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+            // Get forecast with XPath  
+            XmlNodeList nodes = doc.SelectNodes("/rss/channel/item/yweather:forecast", ns);
+
+            try
+            {
+                answer = "Todays weather for " + zip1 + ": " + nodes[0].Attributes["day"].InnerText + ": " +
+                    nodes[0].Attributes["text"].InnerText + ", " +
+                    nodes[0].Attributes["low"].InnerText + "F - " +
+                    nodes[0].Attributes["high"].InnerText + "F\n";
+            }
+            catch (Exception)
+            {
+                answer = "Weather service not currently available!";
+            }
+            return answer;
+        }
     }
 }
